f69b811ef010121be7c27bd0442666b142bef5ca
Naman Chikara <singhnamanchikara@gmail.com>
2019-01-30 14:24:08 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) =>{
-  res.render('index.html')
+  res.render('index')
 })
 // --> 7)  Mount the Logger middleware here
 
