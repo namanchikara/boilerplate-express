75d31d19cc45953e6e9bd7adc88b97b9e68a0976
Naman Chikara <singhnamanchikara@gmail.com>
2019-01-30 14:21:16 +0530

    minor

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) =>{
-  res.render('./views/index.html')
+  res.render('index')
 })
 // --> 7)  Mount the Logger middleware here
 
